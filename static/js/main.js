943b1caed70a05c188ef931e3facf1751843ff54
iamlydial <lydia.lavecchia@gmail.com>
2023-11-12 17:04:29 +0100

    added template tag used to output the current timestap and appended it to the main.js and css file links in order to make sure teh browser fetches them at reload without reusing cached files

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,10 @@
+let chatName = "";
+let chatSocket = null;
+let chatWindowUrl = window.location.href;
+let chatRoomUuid = Math.random().toString(36).slice(2, 12);
+
+console.log("chatRoomUuid", chatRoomUuid);
+
 // elements
 
 const chatElement = document.querySelector("#chat");
@@ -5,6 +12,7 @@ const chatOpenElement = document.querySelector("#chat_open");
 const chatJoinElement = document.querySelector("#chat_join");
 const chatIconElement = document.querySelector("#chat_icon");
 const chatWelcomeElement = document.querySelector("#chat_welcome");
+const chatRoomElement = document.querySelector("#chat_room");
 
 //  event listeners
 
@@ -12,5 +20,12 @@ chatOpenElement.onclick = function (e) {
   e.preventDefault();
   chatIconElement.classList.add("hidden");
   chatWelcomeElement.classList.remove("hidden");
+  return false;
+};
+
+chatJoinElement.onclick = function (e) {
+  e.preventDefault();
+  chatWelcomeElement.classList.add("hidden");
+  chatRoomElement.classList.remove("hidden");
   return false;
 };
