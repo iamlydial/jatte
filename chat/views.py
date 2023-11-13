2420a6e1077fb765a09e406aad5eeafcd2e3aeba
iamlydial <lydia.lavecchia@gmail.com>
2023-11-13 13:14:52 +0100

    created chat url in order to create the room, created the main.js function that creates the room and the admin view

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from .models import Room
+
+@require_POST
+def create_room(request, uuid):
+    name = request.POST.get('name','')
+    url = request.POST.get('url','')
+
+    Room.objects.create(uuid=uuid, client=name, url=url)
+
+    return JsonResponse({'message':'room created'})
--- static/js/main.js
+++ static/js/main.js
@@ -13,6 +13,60 @@ const chatJoinElement = document.querySelector("#chat_join");
 const chatIconElement = document.querySelector("#chat_icon");
 const chatWelcomeElement = document.querySelector("#chat_welcome");
 const chatRoomElement = document.querySelector("#chat_room");
+const chatNameElement = document.querySelector("#chat_name");
+const chatLogElement = document.querySelector("#chat_log");
+const chatInputElement = document.querySelector("#chat_message_input");
+const chatSubmitElement = document.querySelector("#chat_message_submit");
+
+// Functions
+
+function getCookie(name) {
+  var cookieValue = null;
+
+  if (document.cookie && document.cookie != "") {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
+function joinChatRoom() {
+  console.log("joinChatRoom");
+  chatName = chatNameElement.value;
+  console.log("joined as ", chatName);
+  console.log("Room uuid:", chatRoomUuid);
+  const data = new FormData();
+  data.append("name", chatName);
+  data.append("url", chatWindowUrl);
+
+  fetch(`/api/create-room/${chatRoomUuid}/`, {
+    method: "POST",
+    headers: {
+      "X-CSRFToken": getCookie("csrftoken"),
+    },
+    body: data,
+  })
+    .then(function (res) {
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then(function (data) {
+      console.log("data", data);
+    })
+    .catch(function (error) {
+      console.error("Error:", error);
+    });
+}
+
 
 //  event listeners
 
@@ -27,5 +81,8 @@ chatJoinElement.onclick = function (e) {
   e.preventDefault();
   chatWelcomeElement.classList.add("hidden");
   chatRoomElement.classList.remove("hidden");
+
+  joinChatRoom();
+
   return false;
 };
