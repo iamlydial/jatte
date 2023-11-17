5b69552837a177bf9d3824eaddab2ccbc4f350b7
iamlydial <lydia.lavecchia@gmail.com>
2023-11-17 13:11:38 +0100

    make it possibe for managers to create agents

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,10 @@
 import json
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
+from django.contrib import messages
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from account.models import User
 from .models import Room
 from account.forms import AddUserForm, EditUserForm
@@ -43,8 +45,26 @@ def room(request, uuid):
 
 @login_required # decorato restrict to authenticated user only
 def add_user(request): #function that accepts http request as argument 
-    form = AddUserForm() # creates an instance of add user form 
+    if request.user.has_perm('user.add_user'):
+        if request.method == 'POST':
+            form = AddUserForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.is_staff = True
+                user.set_password(request.POST.get('password'))
+                user.save()
 
-    return render(request, 'chat/add_user.html', { # this renders the tempate chat/add_user.html 
-        'form':form # and passes dictionary containing form instance to the template 
-    }) # render function is used to render the html template and return http response 
+                if user.role == User.MANAGER:
+                    group = Group.objects.get(name='Managers')
+                    group.user_set.add(user)
+                
+                messages.success(request, 'The user was added!')
+                return redirect('/chat-admin/')
+        else:
+            form = AddUserForm() # creates an instance of add user form 
+        return render(request, 'chat/add_user.html', { # this renders the tempate chat/add_user.html 
+            'form':form # and passes dictionary containing form instance to the template 
+        }) # render function is used to render the html template and return http response 
+    else:
+        messages.error(request, 'You do not have access to add users!')
+        return redirect('/chat-admin/')
