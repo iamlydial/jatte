a3921a425c65b88dc12637382f3a237a37fb8422
iamlydial <lydia.lavecchia@gmail.com>
2023-11-17 14:08:17 +0100

    make it possible to see the detail view of a user with its rooms

--- chat/views.py
+++ chat/views.py
@@ -43,28 +43,64 @@ def room(request, uuid):
         'room': room
     })
 
-@login_required # decorato restrict to authenticated user only
-def add_user(request): #function that accepts http request as argument 
+@login_required
+def user_detail(request, uuid):
+    user = User.objects.get(pk=uuid)
+    rooms = user.rooms.all()
+    return render(request, 'chat/user_detail.html', {
+        'user': user,
+        'rooms' : rooms
+    })
+    
+
+
+@login_required  # decorator restricts access to authenticated users only
+def add_user(request):  # function that accepts an HTTP request as an argument
+
+    # Check if the authenticated user has permission to add users
     if request.user.has_perm('user.add_user'):
-        if request.method == 'POST':
+
+        # Handling POST request for form submission
+        if request.method == 'POST':  # Check if the request method is POST (form submission)
+
+            # Create a form instance with the POST data received
             form = AddUserForm(request.POST)
-            if form.is_valid():
+
+            if form.is_valid():  # Check if the form data is valid
+
+                # Save the form data to a new user object without committing to the database yet
                 user = form.save(commit=False)
+
+                # Set the 'is_staff' attribute of the user object to True (indicating staff status)
                 user.is_staff = True
+
+                # Set the password for the user based on the input received in the request
                 user.set_password(request.POST.get('password'))
+
+                # Save the user object (with updated attributes) to the database
                 user.save()
 
-                if user.role == User.MANAGER:
-                    group = Group.objects.get(name='Managers')
-                    group.user_set.add(user)
-                
+
+                # Add user to the 'Managers' group if their role is 'Manager'
+                if user.role == User.MANAGER:  # Checking if the user's role is set to 'MANAGER'
+                    group = Group.objects.get(name='Managers')  # Fetching the 'Managers' group from the database
+                    group.user_set.add(user)  # Adding the 'user' to the 'Managers' group
+                    # This associates the 'user' object with the 'Managers' group in the database.
+
+
+                # Display success message and redirect to chat admin page
                 messages.success(request, 'The user was added!')
                 return redirect('/chat-admin/')
-        else:
-            form = AddUserForm() # creates an instance of add user form 
-        return render(request, 'chat/add_user.html', { # this renders the tempate chat/add_user.html 
-            'form':form # and passes dictionary containing form instance to the template 
-        }) # render function is used to render the html template and return http response 
-    else:
+
+        else:  # Handling GET request for displaying form
+            form = AddUserForm()  # creates an instance of the AddUserForm
+
+        # Render the 'chat/add_user.html' template and pass the form instance to the template
+        return render(request, 'chat/add_user.html', {
+            'form': form  # dictionary containing form instance passed to the template
+        })
+
+    else:  # Redirect if the user doesn't have permission to add users
         messages.error(request, 'You do not have access to add users!')
         return redirect('/chat-admin/')
+
